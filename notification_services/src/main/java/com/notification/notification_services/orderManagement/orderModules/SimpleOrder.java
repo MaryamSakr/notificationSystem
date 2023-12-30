416af274bd4f3aab5afc4061ba213fc096acd332
alaa-hossam <alaa.hossam.2013@gmail.com>
2023-12-30 22:08:16 +0200

    edit order2

--- notification_services/src/main/java/com/notification/notification_services/orderManagement/orderModules/SimpleOrder.java
+++ notification_services/src/main/java/com/notification/notification_services/orderManagement/orderModules/SimpleOrder.java
@@ -1,15 +1,10 @@
 package com.notification.notification_services.orderManagement.orderModules;
 import com.notification.notification_services.ProductManagement.ProductModules.Product;
-<<<<<<< HEAD
 import lombok.Getter;
-=======
-
->>>>>>> e2e9aec172082c7213142bcd70b6677aebdae11f
 import java.util.ArrayList;
 import java.util.List;
 @Getter
 public class SimpleOrder extends Order{
-<<<<<<< HEAD
     private List<Product> products;
     private String customerName;
     public SimpleOrder(String customerName, int totalPrice, int shipFees, List<Product> products) {
@@ -31,28 +26,6 @@ public class SimpleOrder extends Order{
     }
 
     public String getCustomerName() {
-        return customerName;
-=======
-    private List<String> products;
-    private int customerId;
-    public SimpleOrder(){
-        super(0);
+        return customerName;}
 
-    }
-    public SimpleOrder(int id, int customerId, List<String> products){
-        super(id);
-        this.customerId = customerId;
-        this.products = new ArrayList<>();
-        this.products = products;
-    }
-    public void setProducts(String product) {
-        this.products.add(product);
-    }
-    public void setProducts(List<String> products) {
-        this.products = products;
-    }
-    public List<String> getProducts() {
-        return products;
->>>>>>> e2e9aec172082c7213142bcd70b6677aebdae11f
-    }
 }
