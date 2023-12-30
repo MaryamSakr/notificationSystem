15dc4028be0272efec5ad2baf1cc92b851d19b41
alaa-hossam <alaa.hossam.2013@gmail.com>
2023-12-30 22:08:16 +0200

    edit order2

--- notification_services/src/main/java/com/notification/notification_services/orderManagement/orderBsl/SimpleOrderBsl.java
+++ notification_services/src/main/java/com/notification/notification_services/orderManagement/orderBsl/SimpleOrderBsl.java
@@ -8,7 +8,6 @@ import com.notification.notification_services.inMemoryOrder;
 import com.notification.notification_services.orderManagement.orderModules.Order;
 import com.notification.notification_services.orderManagement.orderModules.SimpleOrder;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 @Service
 public class SimpleOrderBsl extends OrderBsl{
     private SimpleOrder simpleOrder ;
@@ -20,19 +19,10 @@ public class SimpleOrderBsl extends OrderBsl{
         super();
         this.simpleOrder = simpleOrder;
     }
-=======
-import com.notification.notification_services.inMemory;
-
-@Service
-public class SimpleOrderBsl extends OrderBsl{
-
-    protected SimpleOrder simpleOrder;
->>>>>>> e2e9aec172082c7213142bcd70b6677aebdae11f
     @Override
     public double calcTotal() {
         shipping ship = new shipping();
         double price =0;
-<<<<<<< HEAD
         for (Customer customer : inMemoeryCustomer.customers) {
             if(simpleOrder.getCustomerName().equals(customer.getUserName())){
                 this.flag =true;
@@ -45,21 +35,15 @@ public class SimpleOrderBsl extends OrderBsl{
             return 0;
         }
         simpleOrder.setShipFees(price);
-        for (Product product:simpleOrder.getProducts()) {
-            for (Product pro : inMemory.products ){
-                if(product.getName().equals(pro.getName())){
-=======
         for (Product pro: inMemory.products) {
-            for (String name : simpleOrder.getProducts()) {
-                if (name .equals(pro.getName())) {
->>>>>>> e2e9aec172082c7213142bcd70b6677aebdae11f
+            for (Product product : simpleOrder.getProducts()) {
+                if (product.getName().equals(pro.getName())) {
                     price = price + pro.getPrice();
                 }
             }
         }
         return price;
     }
-<<<<<<< HEAD
     @Override
     public String addOrder(Order o){
         simpleOrder = (SimpleOrder) o;
@@ -70,18 +54,8 @@ public class SimpleOrderBsl extends OrderBsl{
         simpleOrder.setId(inMemoryOrder.orders.get(inMemoryOrder.orders.size()-1).getId() +1 );
         System.out.println(simpleOrder.getId());
         inMemoryOrder.orders.add(o);
-=======
-
-    @Override
-    public String addOrder(Order o) {
-        for(Order order:this.orders) {
-            if(o.getId()==order.getId()){
-                return "This order already exist";
-            }
-        }
-        o.setTotalPrice(calcTotal(o));
-        orders.add(o);
->>>>>>> e2e9aec172082c7213142bcd70b6677aebdae11f
+        o.setTotalPrice(calcTotal());
+        inMemoryOrder.orders.add(o);
         return "Added successfully";
     }
 }
